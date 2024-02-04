7d661a45519325f6e921411b184cad0f5219e5d6
Your Name <igorkol2000@mail.ru>
2024-02-04 14:04:35 +0300

    add bases html and reference

--- ToyStore/ToyStore/settings.py
+++ ToyStore/ToyStore/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+# from django.conf.global_settings import STATICFILES_DIRS не нужен
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -112,6 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- ToyStore/main/views.py
+++ ToyStore/main/views.py
@@ -4,11 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина'
+        'title': 'Home - Главная',
+        'content': 'Магазин игрушек ToyVip',
     }
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About Page')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Добро пожаловать в мир волшебства и веселья - добро пожаловать в мой интернет-магазин ToyVip! '
+                        'Данный интернет-магазин был выбран в качестве дипломного проекта GeekBrains. '
+                        'Спасибо, что посетили сайт магазина ToyVip.'
+    }
+    return render(request, 'main/about.html', context)
