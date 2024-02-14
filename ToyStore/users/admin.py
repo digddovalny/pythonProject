2c69c7ac8f679800c210f582269ac1a9a24b39f7
Your Name <igorkol2000@mail.ru>
2024-02-14 23:31:03 +0300

    add backup bases

--- ToyStore/users/admin.py
+++ ToyStore/users/admin.py
@@ -7,9 +7,6 @@ from baskets.admin import BasketTableAdmin
 from orders.admin import OrderTabulareAdmin
 
 
-# admin.site.register(User)
-# admin.site.register(Products)
-
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'first_name', 'last_name', 'email', ]
